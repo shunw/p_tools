1967cbd7c0e14331c8f735c56ff00c2557f1c7db
Jifeng <jifeng_zhang@arcplus.com.cn>
2018-09-28 20:44:30 +0800

    add module package demo

---
+++ main.py
@@ -0,0 +1,9 @@
+###############
+# @author Jeff
+# @date 2018-09-28
+#######################
+
+import testpackage
+if __name__ == '__main__':
+    print(dir(testpackage))
+    testpackage.helloAgain('Jeff')
---
+++ testpackage/test1.py
@@ -0,0 +1,9 @@
+###############
+# @author Jeff
+# @date 2018-09-28
+#######################
+
+# __all__ = ["concat"]
+
+def hello1(name):
+    print(f"hello1 {name}")
