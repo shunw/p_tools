f238de79ba995f23b043ebf071aeb3cd3998a580
shunw <shunw.li@outlook.com>
2018-10-01 21:26:25 +0800

    add readme with the new content; add init with the scatter plot code

--- pd/README.md
+++ pd/README.md
@@ -1,5 +1,15 @@
 # pandas daily usage
 
-package hierarchy
+## package hierarchy
 
-- `__init__.py` : inherit package name
+- `__init__.py` : inherit package name 
+
+## Note: 
+### Selection:
+- .loc is to choose the label things, 
+    - ex: 
+        - pos = y == 0;
+        - df.loc[pos, df.columns[0]] 
+
+### Plot:
+- 2D scatter plot with different legend
--- pd/__init__.py
+++ pd/__init__.py
@@ -2,3 +2,31 @@
 # @author sw
 # @date 2018-09-28
 #######################
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy import optimize
+
+
+def scatter_plot_2D(X, y):
+    '''
+    do scatter plot (only 2D) and separate the points with different color/ marker/ legend
+    input: X, y (both are dataframe; y define the positive and negative)
+    output: plot
+    '''
+    
+    pos = y==1
+    neg = y==0
+    
+    ad = plt.scatter(X.loc[pos, X.columns[0]], X.loc[pos, X.columns[1]], marker = '^')
+    not_ad = plt.scatter(X.loc[neg, X.columns[0]], X.loc[neg, X.columns[1]], marker = 'o')
+    
+    plt.legend((ad, not_ad), ('admitted', 'not admitted'), loc = 'lower left')
+    
+
+    # # # % Labels and Legend
+    plt.xlabel('Exam 1 score')
+    plt.ylabel('Exam 2 score')
+
+    plt.show()
