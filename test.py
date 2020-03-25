c4b092cc55f6ab7c334482aa5c4eefba450d4a78
shunw <shunw.li@outlook.com>
2020-03-25 10:39:40 +0800

    some changes

---
+++ test.py
@@ -0,0 +1,44 @@
+# To support both python 2 and python 3
+from __future__ import division, print_function, unicode_literals
+
+# Common imports
+import numpy as np
+import os
+
+# to make this notebook's output stable across runs
+np.random.seed(42)
+
+# To plot pretty figures
+# %matplotlib inline
+import matplotlib
+import matplotlib.pyplot as plt
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "decision_trees"
+
+def image_path(fig_id):
+    return os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID, fig_id)
+
+def save_fig(fig_id, tight_layout=True):
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(image_path(fig_id) + ".png", format='png', dpi=300)
+
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+
+iris = load_iris()
+X = iris.data[:, 2:]
+y = iris.target
+
+tree_clf = DecisionTreeClassifier(max_depth = 2)
+tree_clf.fit(X, y)
+
+# export_graphviz(tree_clf, out_file = image_path('iris_tree.dot'), feature_names = iris.feature_names[2:], class_names = iris.target_names, rounded = True, filled = True)
+
+export_graphviz(tree_clf)
